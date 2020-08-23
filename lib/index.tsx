f6457f80cb9b267217318763a47acfe9c7bfe6f2
CFCode-git <435184766@qq.com>
2020-08-23 23:44:54 +0800

    更改tsconfig.json； yarn add --dev cross-env 兼容 windows； 新建webpack.config.prod.js & webpack.config.dev.js

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
 import Button from "./button";
 
-ReactDOM.render(<Button></Button>, document.body);
+const a = 1;
+export { Button, a };
---
+++ webpack.config.dev.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const base = require('./webpack.config')
+module.exports = Object.assign({}, base, {
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'BUI',
+      template: 'index.html'
+    })
+  ],
+})

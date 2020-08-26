eacec7e63581775c63c52e99302825da18d74a45
CFCode-git <435184766@qq.com>
2020-08-26 08:55:34 +0800

    使用解构赋值

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import './importIcons';
 import './Icon.scss';
+import classes from './helpers/classes';
 
-interface IconProps {
+interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string
-  onClick: React.MouseEventHandler<SVGElement>
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-  return (
-    <svg className="bui-icon" onClick={props.onClick}>
-      <use xlinkHref={`#${props.name}`}/>
-    </svg>
-  );
-};
+const Icon: React.FunctionComponent<IconProps> =
+  ({className, name, ...restProps}) => {
+    return (
+      <svg className={classes('bui-icon', className)}
+           {...restProps}
+      >
+        <use xlinkHref={`#${name}`}/>
+      </svg>
+    );
+  };
 
 export default Icon;
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,12 +2,16 @@ import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import Icon from './Icon';
 
-const fn:React.MouseEventHandler = (e) => {
-  console.log(e);
+const fn: React.MouseEventHandler = (e) => {
   console.log(e.target);
 };
 
 ReactDOM.render(<div>
-  <Icon name="qq" onClick={fn}/>
+  <Icon name="qq"
+        className="chow"
+        onClick={fn}
+        onMouseEnter={() => console.log('enter')}
+        onMouseLeave={() => console.log('leave')}
+  />
 </div>, document.querySelector('#root'));
 

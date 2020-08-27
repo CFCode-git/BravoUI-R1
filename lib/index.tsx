7f0e4a3a605f4350809c7e53976d87d6190a1ef9
CFCode-git <435184766@qq.com>
2020-08-27 09:43:01 +0800

    package.json add files; 使用更小的 svg

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,17 +1,3 @@
-import * as ReactDOM from 'react-dom';
-import * as React from 'react';
 import Icon from './Icon/Icon';
 
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-};
-
-ReactDOM.render(<div>
-  <Icon name="qq"
-        className="chow"
-        onClick={fn}
-        onMouseEnter={() => console.log('enter')}
-        onMouseLeave={() => console.log('leave')}
-  />
-</div>, document.querySelector('#root'));
-
+export {Icon};

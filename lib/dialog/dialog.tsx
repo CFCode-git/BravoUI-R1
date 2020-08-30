7ba74c3a1d91d7e462892763121d4d840eb68499
CFCode-git <435184766@qq.com>
2020-08-30 21:12:15 +0800

    完成 dialog 基本功能

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,39 +1,57 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
+import ReactDOM from 'react-dom';
 import './dialog.scss';
 import {Icon} from '../index';
 import {scopedClassMaker} from '../classes';
 
 interface DialogProps {
   visible: boolean
+  buttons: Array<ReactElement>
+  onClose: React.MouseEventHandler
+  closeOnClickMask?: boolean
 }
 
 const scopedClass = scopedClassMaker('bui-dialog');
 const sc = scopedClass;
 
 const Dialog: React.FunctionComponent<DialogProps> = (props) => {
-  return (
-    props.visible ?
-      <React.Fragment>
-        <div className={sc('mask')}/>
-        <div className={sc()}>
-          <div className={sc('close')}>
-            <Icon name="close"/>
-          </div>
-          <header className={sc('header')}>
-            /*TODO 太长有bug */
-            提示
-          </header>
-          <main className={sc('main')}>
-            {props.children}
-          </main>
-          <footer className={sc('footer')}>
-            <button>ok</button>
-            <button>cancel</button>
-          </footer>
+  const onClickClose: React.MouseEventHandler = (e) => {
+    props.onClose(e);
+  };
+  const onClickCloseMask: React.MouseEventHandler = (e) => {
+    if (props.closeOnClickMask) {
+      props.onClose(e);
+    }
+  };
+
+  const x = props.visible ?
+    <React.Fragment>
+      <div className={sc('mask')} onClick={onClickCloseMask}/>
+      <div className={sc()}>
+        <div className={sc('close')} onClick={onClickClose}>
+          <Icon name="close"/>
         </div>
-      </React.Fragment>
-      :
-      null
+        <header className={sc('header')}>
+          {/*TODO 太长有bug */}
+          提示
+        </header>
+        <main className={sc('main')}>
+          {props.children}
+        </main>
+        <footer className={sc('footer')}>
+          {props.buttons.map((button, index) => React.cloneElement(button, {key: index}))}
+        </footer>
+      </div>
+    </React.Fragment>
+    :
+    null;
+  return (
+    ReactDOM.createPortal(x, document.body)
   );
 };
+
+Dialog.defaultProps = {
+  closeOnClickMask: false
+};
+
 export default Dialog;

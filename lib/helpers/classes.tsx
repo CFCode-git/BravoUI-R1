003871f5b2e801047f94140a6be445781071e6a2
CFCode-git <435184766@qq.com>
2020-09-01 15:44:14 +0800

    重构 scopedClassMaker x2

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -6,27 +6,17 @@ export default classes;
 
 interface Options {extra: string | undefined}
 
-interface ClassToggles {
-  [Key: string]: boolean
-}
+interface ClassToggles {[Key: string]: boolean}
 
 function scopedClassMaker(prefix: string) {
-  return function (name: string | ClassToggles, options?: Options) {
-    const namesObject = (typeof name === 'string' || name === undefined) ?
-      {[name]: name} :
-      name;
-    const scoped = Object
-      .entries(namesObject)
+  return (name: string | ClassToggles, options?: Options) => {
+    return Object
+      .entries(name instanceof Object ? name : {[name]: name})
       .filter(kv => kv[1] !== false)
       .map(kv => kv[0])
       .map(name => [prefix, name].filter(Boolean).join('-'))
+      .concat(options && options.extra || [])
       .join(' ');
-
-    if (options && options.extra) {
-      return [scoped, options && options.extra].filter(Boolean).join(' ');
-    } else {
-      return scoped;
-    }
   };
 }
 
